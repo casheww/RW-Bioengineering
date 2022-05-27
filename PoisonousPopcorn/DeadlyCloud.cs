6dd2353b2b8f10d9f7f98af8420686fffb5abee2
casheww <51793722+casheww@users.noreply.github.com>
2022-05-27 17:30:46 +0100

    add poisonous popcorn plants

---
+++ PoisonousPopcorn/DeadlyCloud.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+namespace PoisonousPopcorn;
+
+public class DeadlyCloud : SporeCloud
+{
+    public DeadlyCloud(Vector2 pos, Vector2 vel, Color color, float size, AbstractCreature killTag,
+        int checkInsectsDelay, InsectCoordinator smallInsects, SeedCob cob, float strength = 1.4f)
+        : base(pos, vel, color, size, killTag, checkInsectsDelay, smallInsects)
+    {
+        _cob = cob;
+        _strength = strength;
+    }
+
+    public override void Update(bool eu)
+    {
+        base.Update(eu);
+
+        foreach (AbstractCreature absCreature in room.abstractRoom.creatures)
+        {
+            Creature realCreature = absCreature.realizedCreature;
+
+            if (realCreature is InsectoidCreature ||
+                realCreature.dead ||
+                absCreature.creatureTemplate.type is CreatureTemplate.Type.Spider or CreatureTemplate.Type.Fly)
+                continue;
+            
+            PoisonousPopcornPlugin.nodeManager.Draw($"cloud{Random.value:F3}", Color.cyan, room, room.GetTilePosition(pos), frames: 2);
+
+            float resistance = Mathf.Clamp01(realCreature.TotalMass / _strength);
+            float dist = Mathf.Clamp01(RWCustom.Custom.Dist(pos, realCreature.firstChunk.pos) / (rad * 20f));
+            float prob = Mathf.Cos(dist * Mathf.PI / 2f) * (1 - dist) / 40f;
+            if (Random.value < prob)
+                realCreature.Die();
+            else if (Random.value < prob)
+                realCreature.Violence(_cob.firstChunk, null, realCreature.firstChunk,
+                    null, Creature.DamageType.Explosion, 0.7f, 0.2f);
+        }
+    }
+
+
+    private SeedCob _cob;
+    private float _strength;
+
+}
---
+++ PoisonousPopcorn/PoisonousPopcornPlugin.cs
@@ -0,0 +1,54 @@
+﻿using BepInEx;
+using UnityEngine;
+
+namespace PoisonousPopcorn;
+
+[BepInPlugin("casheww.poisonous_popcorn", nameof(PoisonousPopcorn), "0.1.0")]
+public sealed class PoisonousPopcornPlugin : BaseUnityPlugin
+{
+    public PoisonousPopcornPlugin()
+    {
+        Log = Logger;
+    }
+    
+    private void OnEnable()
+    {
+        On.SeedCob.Open += SeedCob_Open;
+    }
+
+    private void OnDisable()
+    {
+        On.SeedCob.Open -= SeedCob_Open;
+    }
+
+    private void SeedCob_Open(On.SeedCob.orig_Open orig, SeedCob self)
+    {
+        orig(self);
+
+        Vector2 pos = self.AppendagePosition(0, 0);
+        
+        InsectCoordinator smallInsects = null;
+        foreach (UpdatableAndDeletable obj in self.room.updateList)
+        {
+            if (obj is InsectCoordinator insectCoordinator)
+            {
+                smallInsects = insectCoordinator;
+                break;
+            }
+        }
+
+        int cloudCount = (int)Mathf.Lerp(7, 15, Random.value);
+        for (int i = 0; i < cloudCount; i++)
+        {
+            Vector2 vel = new Vector2(Random.value * 2f - 1f, Random.value - 0.7f);
+            self.room.AddObject(
+                new DeadlyCloud(pos, vel, _color, 1.5f, null, 10, smallInsects, self));
+        }
+        self.room.PlaySound(SoundID.Puffball_Eplode, self.firstChunk.pos);
+    }
+
+    private static readonly Color _color = new Color(0.6f, 0.3f, 0.2f);
+    
+    public static BepInEx.Logging.ManualLogSource Log { get; private set; }
+    public static DebuggingHelpers.DebugNodeManager nodeManager = new DebuggingHelpers.DebugNodeManager();
+}
